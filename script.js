7cfb0ee03a826155cf91c5a619d527207afd0978
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-05-26 20:04:31 +0800

    make borders percentages & implement button text scaling

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//sets calculator to 7:10 ratio
 const calculator = document.querySelector("#calculator");
 const  calculatorHeight = calculator.offsetHeight;
 calculator.style.cssText = `width: ${0.7*calculatorHeight}px`
+
+//uses height of button seven to set the font to half the button container
+const buttons = document.querySelectorAll(".buttons")
+const seven = document.querySelector("#seven")
+const buttonHeight = seven.offsetHeight;
+buttons.forEach(e => e.style.fontSize = `${buttonHeight/2}px`);
